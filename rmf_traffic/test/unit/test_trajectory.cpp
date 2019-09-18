5d0ceb88705d89be5c003b1f7069cb4d11c2adbb
Boon Han <cnboonhan94@gmail.com>
2019-09-18 10:48:18 +0800

    refactor tests for readability

--- rmf_traffic/test/unit/test_trajectory.cpp
+++ rmf_traffic/test/unit/test_trajectory.cpp
@@ -22,12 +22,26 @@
 #include <rmf_utils/catch.hpp>
 #include <iostream>
 
+rmf_traffic::Trajectory::ProfilePtr make_test_profile(std::string shape)
+{
+  if (shape == "unit_box")
+  {
+    return rmf_traffic::Trajectory::Profile::make_strict(
+      std::make_shared<rmf_traffic::geometry::Box>(1.0, 1.0));
+  } else if (shape == "unit_circle")
+  {
+    return rmf_traffic::Trajectory::Profile::make_strict(
+      std::make_shared<rmf_traffic::geometry::Circle>(1.0));
+  } else {
+    REQUIRE(false);
+    return 0;
+  }
+}
+
 SCENARIO("Class Profile unit tests")
 {
-  // The main properties we are concerned in Profile are the getting and setting of
-  // Movement ( Strict / Autonomous / Queued ) and profile Shape
 
-  GIVEN("Shape: Unit Square, Movement: Strict. Checking Accessor Functions")
+  GIVEN("Checking Accessor Functions")
   {
     std::shared_ptr<rmf_traffic::geometry::Box> profile_shape = std::make_shared<rmf_traffic::geometry::Box>(1.0, 1.0);
     rmf_traffic::Trajectory::ProfilePtr profile = rmf_traffic::Trajectory::Profile::make_strict(profile_shape);
@@ -64,159 +78,160 @@ SCENARIO("Class Profile unit tests")
 
   GIVEN("Shape: Unit Square, Movement: Strict. Checking Dirty Input")
   {
-    std::shared_ptr<rmf_traffic::geometry::Box> profile_shape = std::make_shared<rmf_traffic::geometry::Box>(1.0, 1.0);
-    rmf_traffic::Trajectory::ProfilePtr profile = rmf_traffic::Trajectory::Profile::make_strict(profile_shape);
-    
+    rmf_traffic::Trajectory::ProfilePtr profile = make_test_profile("unit_box");
+
     WHEN("Giving NULL as queue_id is not allowed")
     {
       CHECK_THROWS(profile->set_to_queued(NULL));
     }
   }
 }
-  // rmf_traffic::Trajectory::ProfilePtr make_test_profile()
-  // {
-  //   return rmf_traffic::Trajectory::Profile::make_strict(
-  //         std::make_shared<rmf_traffic::geometry::Box>(1.0, 1.0));
-  // }
 
-  // TEST_CASE("Construct a Trajectory")
-  // {
-  //   using namespace std::chrono_literals;
 
-  //   rmf_traffic::Trajectory trajectory{"test_map"};
-  //   CHECK(trajectory.begin() == trajectory.end());
-  //   CHECK(trajectory.end() == trajectory.end());
+// rmf_traffic::Trajectory::ProfilePtr make_test_profile()
+// {
+//   return rmf_traffic::Trajectory::Profile::make_strict(
+//         std::make_shared<rmf_traffic::geometry::Box>(1.0, 1.0));
+// }
 
-  //   const auto profile = make_test_profile();
+// TEST_CASE("Construct a Trajectory")
+// {
+//   using namespace std::chrono_literals;
 
-  //   const auto begin_time = std::chrono::steady_clock::now();
-  //   const Eigen::Vector3d begin_p = Eigen::Vector3d(0, 0, 0);
-  //   const Eigen::Vector3d begin_v = Eigen::Vector3d(0, 0, 0);
+//   rmf_traffic::Trajectory trajectory{"test_map"};
+//   CHECK(trajectory.begin() == trajectory.end());
+//   CHECK(trajectory.end() == trajectory.end());
 
-  //   auto result = trajectory.insert(
-  //         begin_time, profile, begin_p, begin_v);
+//   const auto profile = make_test_profile();
 
-  //   const rmf_traffic::Trajectory::iterator begin_it = result.it;
-  //   CHECK(result.inserted);
+//   const auto begin_time = std::chrono::steady_clock::now();
+//   const Eigen::Vector3d begin_p = Eigen::Vector3d(0, 0, 0);
+//   const Eigen::Vector3d begin_v = Eigen::Vector3d(0, 0, 0);
 
-  //   CHECK(begin_it == trajectory.begin());
-  //   CHECK(trajectory.begin() != trajectory.end());
-  //   CHECK(begin_it != trajectory.end());
-  //   CHECK(begin_it < trajectory.end());
-  //   CHECK(begin_it <= trajectory.end());
-  //   CHECK(trajectory.end() > begin_it);
-  //   CHECK(trajectory.end() >= trajectory.end());
+//   auto result = trajectory.insert(
+//         begin_time, profile, begin_p, begin_v);
 
-  //   CHECK(begin_p == begin_it->get_position());
-  //   CHECK(begin_v == begin_it->get_velocity());
-  //   CHECK(begin_time == begin_it->get_finish_time());
+//   const rmf_traffic::Trajectory::iterator begin_it = result.it;
+//   CHECK(result.inserted);
 
-  //   const auto second_time = begin_time + 10s;
-  //   const Eigen::Vector3d second_p = Eigen::Vector3d(1, 2, 3);
-  //   const Eigen::Vector3d second_v = Eigen::Vector3d(3, 2, 1);
+//   CHECK(begin_it == trajectory.begin());
+//   CHECK(trajectory.begin() != trajectory.end());
+//   CHECK(begin_it != trajectory.end());
+//   CHECK(begin_it < trajectory.end());
+//   CHECK(begin_it <= trajectory.end());
+//   CHECK(trajectory.end() > begin_it);
+//   CHECK(trajectory.end() >= trajectory.end());
 
-  //   result = trajectory.insert(
-  //         second_time, profile, second_p, second_v);
+//   CHECK(begin_p == begin_it->get_position());
+//   CHECK(begin_v == begin_it->get_velocity());
+//   CHECK(begin_time == begin_it->get_finish_time());
 
-  //   const rmf_traffic::Trajectory::iterator second_it = result.it;
-  //   CHECK(result.inserted);
+//   const auto second_time = begin_time + 10s;
+//   const Eigen::Vector3d second_p = Eigen::Vector3d(1, 2, 3);
+//   const Eigen::Vector3d second_v = Eigen::Vector3d(3, 2, 1);
 
-  //   CHECK(second_it == ++trajectory.begin());
-  //   CHECK(second_it != trajectory.begin());
-  //   CHECK(second_it > trajectory.begin());
-  //   CHECK(second_it >= trajectory.begin());
-  //   CHECK(trajectory.begin() < second_it);
-  //   CHECK(trajectory.begin() <= second_it);
+//   result = trajectory.insert(
+//         second_time, profile, second_p, second_v);
 
-  //   CHECK(second_it != begin_it);
-  //   CHECK(second_it > begin_it);
-  //   CHECK(second_it >= begin_it);
-  //   CHECK(begin_it < second_it);
-  //   CHECK(begin_it <= second_it);
+//   const rmf_traffic::Trajectory::iterator second_it = result.it;
+//   CHECK(result.inserted);
 
-  //   CHECK(second_it != trajectory.end());
-  //   CHECK(second_it < trajectory.end());
-  //   CHECK(second_it <= trajectory.end());
-  //   CHECK(trajectory.end() > second_it);
-  //   CHECK(trajectory.end() >= second_it);
+//   CHECK(second_it == ++trajectory.begin());
+//   CHECK(second_it != trajectory.begin());
+//   CHECK(second_it > trajectory.begin());
+//   CHECK(second_it >= trajectory.begin());
+//   CHECK(trajectory.begin() < second_it);
+//   CHECK(trajectory.begin() <= second_it);
 
-  //   CHECK(second_it->get_position() == second_p);
-  //   CHECK(second_it->get_velocity() == second_v);
-  //   CHECK(second_it->get_finish_time() == second_time);
-  // }
+//   CHECK(second_it != begin_it);
+//   CHECK(second_it > begin_it);
+//   CHECK(second_it >= begin_it);
+//   CHECK(begin_it < second_it);
+//   CHECK(begin_it <= second_it);
 
-  // TEST_CASE("Copy and move a trajectory")
-  // {
-  //   using namespace std::chrono_literals;
+//   CHECK(second_it != trajectory.end());
+//   CHECK(second_it < trajectory.end());
+//   CHECK(second_it <= trajectory.end());
+//   CHECK(trajectory.end() > second_it);
+//   CHECK(trajectory.end() >= second_it);
 
-  //   rmf_traffic::Trajectory trajectory{"test_map"};
+//   CHECK(second_it->get_position() == second_p);
+//   CHECK(second_it->get_velocity() == second_v);
+//   CHECK(second_it->get_finish_time() == second_time);
+// }
 
-  //   const auto begin_time = std::chrono::steady_clock::now();
+// TEST_CASE("Copy and move a trajectory")
+// {
+//   using namespace std::chrono_literals;
 
-  //   trajectory.insert(
-  //         begin_time, make_test_profile(),
-  //         Eigen::Vector3d::UnitX(),
-  //         Eigen::Vector3d::UnitX());
+//   rmf_traffic::Trajectory trajectory{"test_map"};
 
-  //   trajectory.insert(
-  //         begin_time + 10s, make_test_profile(),
-  //         Eigen::Vector3d::UnitY(),
-  //         Eigen::Vector3d::UnitY());
+//   const auto begin_time = std::chrono::steady_clock::now();
 
-  //   trajectory.insert(
-  //         begin_time + 15s, make_test_profile(),
-  //         Eigen::Vector3d::UnitZ(),
-  //         Eigen::Vector3d::UnitZ());
+//   trajectory.insert(
+//         begin_time, make_test_profile(),
+//         Eigen::Vector3d::UnitX(),
+//         Eigen::Vector3d::UnitX());
 
-  //   rmf_traffic::Trajectory copy = trajectory;
+//   trajectory.insert(
+//         begin_time + 10s, make_test_profile(),
+//         Eigen::Vector3d::UnitY(),
+//         Eigen::Vector3d::UnitY());
 
-  //   rmf_traffic::Trajectory::const_iterator ot = trajectory.begin();
-  //   rmf_traffic::Trajectory::const_iterator ct = copy.begin();
-  //   for( ; ot != trajectory.end() && ct != trajectory.end(); ++ot, ++ct)
-  //   {
-  //     CHECK(ot->get_profile() == ct->get_profile());
-  //     CHECK(ot->get_position() == ct->get_position());
-  //     CHECK(ot->get_velocity() == ct->get_velocity());
-  //     CHECK(ot->get_finish_time() == ct->get_finish_time());
-  //   }
-  //   CHECK(ot == trajectory.end());
-  //   CHECK(ct == copy.end());
+//   trajectory.insert(
+//         begin_time + 15s, make_test_profile(),
+//         Eigen::Vector3d::UnitZ(),
+//         Eigen::Vector3d::UnitZ());
 
-  //   for(auto it = copy.begin(); it != copy.end(); ++it)
-  //   {
-  //     it->set_profile(make_test_profile());
-  //     it->set_position(it->get_position() + Eigen::Vector3d::UnitZ());
-  //     it->set_velocity(it->get_velocity() + Eigen::Vector3d::UnitZ());
-  //     it->set_finish_time(it->get_finish_time() + 2s);
-  //   }
+//   rmf_traffic::Trajectory copy = trajectory;
 
-  //   ot = trajectory.begin();
-  //   ct = copy.begin();
-  //   for( ; ot != trajectory.end() && ct != trajectory.end(); ++ot, ++ct)
-  //   {
-  //     CHECK(ot->get_profile() != ct->get_profile());
-  //     CHECK(ot->get_position() != ct->get_position());
-  //     CHECK(ot->get_velocity() != ct->get_velocity());
-  //     CHECK(ot->get_finish_time() != ct->get_finish_time());
-  //   }
-  //   CHECK(ot == trajectory.end());
-  //   CHECK(ct == copy.end());
+//   rmf_traffic::Trajectory::const_iterator ot = trajectory.begin();
+//   rmf_traffic::Trajectory::const_iterator ct = copy.begin();
+//   for( ; ot != trajectory.end() && ct != trajectory.end(); ++ot, ++ct)
+//   {
+//     CHECK(ot->get_profile() == ct->get_profile());
+//     CHECK(ot->get_position() == ct->get_position());
+//     CHECK(ot->get_velocity() == ct->get_velocity());
+//     CHECK(ot->get_finish_time() == ct->get_finish_time());
+//   }
+//   CHECK(ot == trajectory.end());
+//   CHECK(ct == copy.end());
 
-  //   // Copy again
-  //   copy = trajectory;
+//   for(auto it = copy.begin(); it != copy.end(); ++it)
+//   {
+//     it->set_profile(make_test_profile());
+//     it->set_position(it->get_position() + Eigen::Vector3d::UnitZ());
+//     it->set_velocity(it->get_velocity() + Eigen::Vector3d::UnitZ());
+//     it->set_finish_time(it->get_finish_time() + 2s);
+//   }
 
-  //   // Now move the original
-  //   rmf_traffic::Trajectory moved = std::move(trajectory);
+//   ot = trajectory.begin();
+//   ct = copy.begin();
+//   for( ; ot != trajectory.end() && ct != trajectory.end(); ++ot, ++ct)
+//   {
+//     CHECK(ot->get_profile() != ct->get_profile());
+//     CHECK(ot->get_position() != ct->get_position());
+//     CHECK(ot->get_velocity() != ct->get_velocity());
+//     CHECK(ot->get_finish_time() != ct->get_finish_time());
+//   }
+//   CHECK(ot == trajectory.end());
+//   CHECK(ct == copy.end());
 
-  //   ct = copy.begin();
-  //   rmf_traffic::Trajectory::const_iterator mt = moved.begin();
-  //   for( ; ct != copy.end() && mt != moved.end(); ++ct, ++mt)
-  //   {
-  //     CHECK(ct->get_profile() == mt->get_profile());
-  //     CHECK(ct->get_position() == mt->get_position());
-  //     CHECK(ct->get_velocity() == mt->get_velocity());
-  //     CHECK(ct->get_finish_time() == mt->get_finish_time());
-  //   }
-  //   CHECK(ct == copy.end());
-  //   CHECK(mt == moved.end());
-  // }
+//   // Copy again
+//   copy = trajectory;
+
+//   // Now move the original
+//   rmf_traffic::Trajectory moved = std::move(trajectory);
+
+//   ct = copy.begin();
+//   rmf_traffic::Trajectory::const_iterator mt = moved.begin();
+//   for( ; ct != copy.end() && mt != moved.end(); ++ct, ++mt)
+//   {
+//     CHECK(ct->get_profile() == mt->get_profile());
+//     CHECK(ct->get_position() == mt->get_position());
+//     CHECK(ct->get_velocity() == mt->get_velocity());
+//     CHECK(ct->get_finish_time() == mt->get_finish_time());
+//   }
+//   CHECK(ct == copy.end());
+//   CHECK(mt == moved.end());
+// }
