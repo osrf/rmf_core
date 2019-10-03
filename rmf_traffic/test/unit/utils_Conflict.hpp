d2d6e8028067f9157f0e213227e4249681ebdedc
Boon Han <cnboonhan94@gmail.com>
2019-10-03 13:19:45 +0800

    finish up narrow phase commutative checks

--- rmf_traffic/test/unit/utils_Conflict.hpp
+++ rmf_traffic/test/unit/utils_Conflict.hpp
@@ -32,13 +32,25 @@ inline void CHECK_broad_phase_is_commutative(rmf_traffic::Trajectory t1, rmf_tra
 inline void CHECK_narrow_phase_is_commutative(rmf_traffic::Trajectory t1, rmf_traffic::Trajectory t2)
 {
     REQUIRE(rmf_traffic::DetectConflict::broad_phase(t1, t2));
-    const auto conflicts_1 = rmf_traffic::DetectConflict::narrow_phase(t1, t2);
-    const auto conflicts_2 = rmf_traffic::DetectConflict::narrow_phase(t2, t1);
+    auto conflicts_1 = rmf_traffic::DetectConflict::narrow_phase(t1, t2);
+    auto conflicts_2 = rmf_traffic::DetectConflict::narrow_phase(t2, t1);
     REQUIRE(conflicts_1.size() == conflicts_2.size());
     for (auto i_1 = conflicts_1.begin(), i_2 = conflicts_2.begin(); i_1 != conflicts_1.end(); i_1++, i_2++)
     {
         CHECK(i_1->get_time() == i_2->get_time());
-        // add iterative checks
+        rmf_traffic::Trajectory::const_iterator conflict_1_t1 =  i_1->get_segments().first;
+        rmf_traffic::Trajectory::const_iterator conflict_2_t1 =  i_2->get_segments().second;
+        CHECK(conflict_1_t1->get_profile() == conflict_2_t1->get_profile());
+        CHECK(conflict_1_t1->get_finish_position() == conflict_2_t1->get_finish_position());
+        CHECK(conflict_1_t1->get_finish_velocity() == conflict_2_t1->get_finish_velocity());
+        CHECK(conflict_1_t1->get_finish_time() == conflict_2_t1->get_finish_time());
+
+        rmf_traffic::Trajectory::const_iterator conflict_1_t2 =  i_1->get_segments().second;
+        rmf_traffic::Trajectory::const_iterator conflict_2_t2 =  i_2->get_segments().first;
+        CHECK(conflict_1_t2->get_profile() == conflict_2_t2->get_profile());
+        CHECK(conflict_1_t2->get_finish_position() == conflict_2_t2->get_finish_position());
+        CHECK(conflict_1_t2->get_finish_velocity() == conflict_2_t2->get_finish_velocity());
+        CHECK(conflict_1_t2->get_finish_time() == conflict_2_t2->get_finish_time());
     }
 }
 
