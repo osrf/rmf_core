76c13d123b75b96e58c589a5726d0e8500d62735
Yadunund <yadunund@openrobotics.org>
2019-10-11 17:05:31 +0800

    Added basic tests for Databse member functions

--- rmf_traffic/test/unit/schedule/test_Database.cpp
+++ rmf_traffic/test/unit/schedule/test_Database.cpp
@@ -34,41 +34,159 @@ SCENARIO("Test Database Conflicts")
         rmf_traffic::schedule::Database db;
 
         //check for empty instantiation
-        const rmf_traffic::schedule::Query query_param= rmf_traffic::schedule::query_everything();
-        rmf_traffic::schedule::Database::Patch changes= db.changes(query_param);
-        REQUIRE(changes.latest_version()==0);
-        CHECK(changes.size()==0);
-        for(auto x :changes)
+        const rmf_traffic::schedule::Query query_everything= rmf_traffic::schedule::query_everything();
+        rmf_traffic::schedule::Database::Patch changes= db.changes(query_everything);
+        REQUIRE(changes.size()==0);
+      /*  for(auto x :changes)
         {
             std::cout<<static_cast<int>(x.get_mode())<<std::endl;
             std::cout<<x.cull()->time().time_since_epoch().count()<<std::endl;
 
-        }
+        }*/
         //adding simple two-point trajecotry
+        const double profile_scale = 1.0;
+        const rmf_traffic::Time time = std::chrono::steady_clock::now();
+        rmf_traffic::geometry::Box shape(profile_scale,profile_scale);
+        rmf_traffic::geometry::FinalConvexShapePtr final_shape= rmf_traffic::geometry::make_final_convex(shape);
+        rmf_traffic::Trajectory::ProfilePtr profile = rmf_traffic::Trajectory::Profile::make_strict(final_shape);
         rmf_traffic::Trajectory t1("test_map");
-        rmf_traffic::Trajectory::ProfilePtr profile=
+        t1.insert(time, profile, Eigen::Vector3d{-5,0,0}, Eigen::Vector3d{0,0,0});
+        t1.insert(time + 10s, profile, Eigen::Vector3d{5,0,0}, Eigen::Vector3d{0,0,0});
+        REQUIRE(t1.size()==2);
+
+        rmf_traffic::schedule::Version version=  db.insert(t1);
+        REQUIRE(version==1);
+        //create a query after_version 0
+        auto query= rmf_traffic::schedule::make_query(0);
+        changes=db.changes(query);
+        CHECK(changes.size()==1); //currently failing 
+        CHECK(changes.latest_version()==1);
+        for(auto change :changes)
+              {
+
+                CHECK(static_cast<int>(change.get_mode())==1); //MODE:Insert=1
+                CHECK(change.id()==1);
+                CHECK(change.insert()!=nullptr);
+              }
+            
     
-        WHEN("Trajectory is inserted")
+        WHEN("Another Trajectory is inserted")
         {
-            //check changes
+            rmf_traffic::Trajectory t2("test_map");
+            t2.insert(time,profile, Eigen::Vector3d{0,-5,0},Eigen::Vector3d{0,0,0});
+            t2.insert(time+10s,profile, Eigen::Vector3d{0,5,0},Eigen::Vector3d{0,0,0});
+            REQUIRE(t2.size()==2);
+
+            rmf_traffic::schedule::Version version2= db.insert(t2);
+            REQUIRE(version2==2);
+            changes=db.changes(query_everything);
+            REQUIRE(changes.size()==2);
+            REQUIRE(changes.latest_version()==2);
+            //get the latest query
+            query=rmf_traffic::schedule::make_query(1);
+            changes=db.changes(query);
+            REQUIRE(changes.size()==1);//only one change made since first insert
+            REQUIRE(changes.begin()->id()==2);
+            REQUIRE(static_cast<int>(changes.begin()->get_mode())==1);
+            REQUIRE(changes.begin()->insert()!=nullptr);
+
         }
 
         WHEN("Trajectory is interrupted")
         {
 
+          rmf_traffic::Trajectory t2("test_map");
+          t2.insert(time+5s,profile, Eigen::Vector3d{0,1,0},Eigen::Vector3d{0,0,0});
+          t2.insert(time+5s,profile, Eigen::Vector3d{0,1,0},Eigen::Vector3d{0,0,0});
+          REQUIRE(t2.size()==2);
+          rmf_traffic::schedule::Version version2= db.interrupt(1,t2,0s);
+          CHECK(version2==2);
+          changes=db.changes(query_everything);
+          REQUIRE(changes.size()==2);
+          REQUIRE(changes.latest_version()==2);
+          auto interrupt_change= changes.end();
+          REQUIRE(static_cast<int>(interrupt_change->get_mode())==2);
+          REQUIRE(interrupt_change->interrupt()!=nullptr);
+          auto interrupt=interrupt_change->interrupt();
+          CHECK(interrupt->original_id()==1);
+          CHECK(interrupt->interruption()==&t2); //INVALID. Write function to compare
+
+          //check for number of trajectories in db
+  
+
+        }
+
+        WHEN("Trajectory is delayed")
+        {
+          //introduce a delay after the first segment in t1
+          rmf_traffic::schedule::Version version2= db.delay(1,time,5s);
+          CHECK(version2==2);
+          changes=db.changes(query_everything);
+          REQUIRE(changes.size()==2);
+          REQUIRE(changes.latest_version()==2);
+          auto delay_change= changes.end();
+          REQUIRE(static_cast<int>(delay_change->get_mode())==3);
+          REQUIRE(delay_change->delay()!=nullptr);
+          auto delay=delay_change->delay();
+          CHECK(delay->original_id()==1);
+          CHECK(delay->from()==time);
+          CHECK(delay->duration()==5s);
+          
         }
 
         WHEN("Trajectory is replaced")
         {
+          rmf_traffic::Trajectory t2("test_map");
+          t2.insert(time+5s,profile, Eigen::Vector3d{0,1,0},Eigen::Vector3d{0,0,0});
+          t2.insert(time+5s,profile, Eigen::Vector3d{0,1,0},Eigen::Vector3d{0,0,0});
+          REQUIRE(t2.size()==2);
+
+          rmf_traffic::schedule::Version version2= db.replace(1,t2);
+          CHECK(version2==2);
+          changes=db.changes(query_everything);
+          REQUIRE(changes.size()==2);
+          REQUIRE(changes.latest_version()==2);
+          auto replace_change= changes.end();
+          REQUIRE(static_cast<int>(replace_change->get_mode())==4);
+          REQUIRE(replace_change->replace()!=nullptr);
+          auto replace=replace_change->replace();
+          CHECK(replace->original_id()==1);
+          REQUIRE(replace->trajectory()!=nullptr);
+
+          CHECK(replace->trajectory()==&t2); //CHANGE
+
+          
 
         }
 
         WHEN("Trajectory is erased")
         {
+
+          rmf_traffic::schedule::Version version2= db.erase(1);
+          CHECK(version2==2);
+          changes=db.changes(query_everything);
+          REQUIRE(changes.size()==2);
+          REQUIRE(changes.latest_version()==2);
+          auto erase_change= changes.end();
+          REQUIRE(static_cast<int>(erase_change->get_mode())==5);
+          REQUIRE(erase_change->erase()!=nullptr);
+          auto erase=erase_change->erase();
+          CHECK(erase->original_id()==1);
             
         }
         WHEN("Trajectory is culled")
         {
+          auto cull_time=time+20s;
+          rmf_traffic::schedule::Version version2= db.cull(cull_time);
+          CHECK(version2==2);
+          changes=db.changes(query_everything);
+          REQUIRE(changes.size()==2);
+          REQUIRE(changes.latest_version()==2);
+          auto cull_change= changes.end();
+          REQUIRE(static_cast<int>(cull_change->get_mode())==6);
+          REQUIRE(cull_change->cull()!=nullptr);
+          auto cull=cull_change->cull();
+          CHECK(cull->time()==cull_time);
             
         }
 
