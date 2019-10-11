b4bc365fa611640e94a4304798252a7f9468c861
Yadunund <yadunund@openrobotics.org>
2019-10-11 17:20:48 +0800

    corrected trajectory definition

---
+++ rmf_traffic/test/unit/schedule/utils_Database.hpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2019 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#ifndef RMF_TRAFFIC__TEST__UNIT__SCHEDULE__UTILS_TRAJECTORY_HPP
+#define RMF_TRAFFIC__TEST__UNIT__SCHEDULE__UTILS_TRAJECTORY_HPP
+
+#include <rmf_traffic/Trajectory.hpp>
+#include <rmf_traffic/geometry/Box.hpp>
+#include <rmf_traffic/geometry/Circle.hpp>
+#include <rmf_utils/catch.hpp>
+
+inline void CHECK_TRAJECTORY_IS_EQUAL(rmf_traffic::Trajectory t1, rmf_traffic::Trajectory t2)
+{
+
+REQUIRE(t1.size()==t2.size());
+
+for(auto it1=t1.begin(),it2=t2.begin();it1!=t1.end();it1++,it2++)
+{
+
+
+    
+}
+
+
+
+}
+
+
+
+
+
+#endif //RMF_TRAFFIC__TEST__UNIT__SCHEDULE__UTILS_TRAJECTORY_HPP
