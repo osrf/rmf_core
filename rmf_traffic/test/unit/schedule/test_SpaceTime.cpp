76ecbab43d29eb80bf66287321e15f6e98caf5ab
Yadunund <yadunund@openrobotics.org>
2019-10-15 14:36:35 +0800

    spacetime conflict tests are passing

--- rmf_traffic/test/unit/schedule/test_SpaceTime.cpp
+++ rmf_traffic/test/unit/schedule/test_SpaceTime.cpp
@@ -15,31 +15,44 @@
 SCENARIO("Testing intersaection of spacetime with trajectories")
 {
   using namespace std::chrono_literals;
-
-
-  std::cout<<"Testing spacetime\n";
   auto time = std::chrono::steady_clock::now();
   Eigen::Isometry2d tf = Eigen::Isometry2d::Identity();
 
-
-  //creating space to add to region
+  //creating space to create spacetime
   const auto box = rmf_traffic::geometry::Box(10.0, 1.0);
   const auto final_box = rmf_traffic::geometry::make_final_convex(box);
 
-  std::chrono::steady_clock::time_point* lower_time_bound;
-  std::chrono::steady_clock::time_point* upper_time_bound;
-  *lower_time_bound=time;
-  *upper_time_bound=time+10s;
-
+  std::chrono::_V2::steady_clock::time_point lower_time_bound=time;
+  std::chrono::_V2::steady_clock::time_point upper_time_bound=time+10s;
 
   rmf_traffic::internal::Spacetime region={
-    lower_time_bound,
-    upper_time_bound,
+    &lower_time_bound,
+    &upper_time_bound,
     tf,
     final_box
   };
 
 
+  GIVEN("A box trajectory that does not intersect with the spacetime region")
+{
+
+    //creating trajectory
+  rmf_traffic::geometry::Box shape(1,1);
+  rmf_traffic::geometry::FinalConvexShapePtr final_shape= rmf_traffic::geometry::make_final_convex(shape);
+  rmf_traffic::Trajectory::ProfilePtr profile = rmf_traffic::Trajectory::Profile::make_strict(final_shape);
+  rmf_traffic::Trajectory t1("test_map");
+
+  t1.insert(time, profile, Eigen::Vector3d{-5,10,0},Eigen::Vector3d{5,0,0});
+  t1.insert(time+10s, profile, Eigen::Vector3d{5,10,0},Eigen::Vector3d{5,0,0});
+  REQUIRE(t1.size()==2);
+
+
+  std::vector<rmf_traffic::Trajectory::const_iterator> output_iterators;
+  CHECK_FALSE(rmf_traffic::internal::detect_conflicts(t1,region,&output_iterators));
+}
+
+  GIVEN("A box trajectory that completely intersects with the spacetime region")
+  {
   //creating trajectory
   rmf_traffic::geometry::Box shape(1,1);
   rmf_traffic::geometry::FinalConvexShapePtr final_shape= rmf_traffic::geometry::make_final_convex(shape);
@@ -51,8 +64,27 @@ SCENARIO("Testing intersaection of spacetime with trajectories")
   REQUIRE(t1.size()==2);
 
 
-  std::vector<rmf_traffic::Trajectory::const_iterator>* output_iterators;
-  REQUIRE(rmf_traffic::internal::detect_conflicts(t1,region,output_iterators));
+  std::vector<rmf_traffic::Trajectory::const_iterator> output_iterators;
+  CHECK(rmf_traffic::internal::detect_conflicts(t1,region,&output_iterators));
+  }
+
+  GIVEN("A box trajectory that partially intersects with the spacetime region")
+  {
+  //creating trajectory
+  rmf_traffic::geometry::Box shape(1,1);
+  rmf_traffic::geometry::FinalConvexShapePtr final_shape= rmf_traffic::geometry::make_final_convex(shape);
+  rmf_traffic::Trajectory::ProfilePtr profile = rmf_traffic::Trajectory::Profile::make_strict(final_shape);
+  rmf_traffic::Trajectory t1("test_map");
+
+  t1.insert(time, profile, Eigen::Vector3d{-1,0,0},Eigen::Vector3d{5,0,0});
+  t1.insert(time+10s, profile, Eigen::Vector3d{1,0,0},Eigen::Vector3d{5,0,0});
+  REQUIRE(t1.size()==2);
+
+
+  std::vector<rmf_traffic::Trajectory::const_iterator> output_iterators;
+  CHECK(rmf_traffic::internal::detect_conflicts(t1,region,&output_iterators));
+  }
+
 
 
 
