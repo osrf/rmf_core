03fceb91ae35fd11cbdafe4730f057df535672fe
Michael X. Grey <grey@openrobotics.org>
2020-03-29 21:06:20 +0800

    Printing debug info for schedule negotiation

--- rmf_traffic_ros2/src/rmf_traffic_schedule/ScheduleNode.cpp
+++ rmf_traffic_ros2/src/rmf_traffic_schedule/ScheduleNode.cpp
@@ -623,7 +623,43 @@ void ScheduleNode::receive_proposal(const ConflictProposal& msg)
 
   table->submit(rmf_traffic_ros2::convert(msg.itinerary), msg.proposal_version);
 
-  print the proposals so far
+  std::vector<Negotiation::ConstTablePtr> terminal;
+  std::vector<Negotiation::ConstTablePtr> queue;
+  for (const auto p : negotiation->participants())
+    queue.push_back(negotiation->table(p, {}));
+
+  while (!queue.empty())
+  {
+    Negotiation::ConstTablePtr top = queue.back();
+    queue.pop_back();
+
+    std::vector<Negotiation::ConstTablePtr> children = top->children();
+    if (children.empty())
+    {
+      terminal.push_back(top);
+    }
+    else
+    {
+      for (const auto& child : children)
+        queue.push_back(child);
+    }
+  }
+
+  std::cout << "\nCurrent negotiation state:";
+  for (const auto& t : terminal)
+  {
+    std::cout << "\n --";
+    const bool finished = static_cast<bool>(t->submission());
+    const auto sequence = t->sequence();
+    for (std::size_t i=0; i < sequence.size(); ++i)
+    {
+      if (i == t->sequence().size()-1 && !finished)
+        std::cout << " [" << sequence[i] << "]";
+      else
+        std::cout << " " << sequence[i];
+    }
+  }
+  std::cout << "\n" << std::endl;
 
   if (negotiation->ready())
   {
