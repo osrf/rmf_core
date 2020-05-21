5d972377a26f5d23c517123f8eb56f3d768aa259
Michael X. Grey <grey@openrobotics.org>
2020-05-21 17:00:41 +0800

    More implementation

---
+++ rmf_fleet_adapter/src/rmf_fleet_adapter/agv/RobotContext.cpp
@@ -0,0 +1,151 @@
+/*
+ * Copyright (C) 2020 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include "internal_RobotUpdateHandle.hpp"
+
+#include <rmf_traffic_ros2/Time.hpp>
+
+namespace rmf_fleet_adapter {
+namespace agv {
+
+//==============================================================================
+std::shared_ptr<RobotCommandHandle> RobotContext::command()
+{
+  return _command_handle.lock();
+}
+
+//==============================================================================
+std::shared_ptr<RobotUpdateHandle> RobotContext::make_updater()
+{
+  return RobotUpdateHandle::Implementation::make(shared_from_this());
+}
+
+//==============================================================================
+Eigen::Vector3d RobotContext::position() const
+{
+  assert(!_location.empty());
+  const auto& l = _location.front();
+  if (l.location().has_value())
+  {
+    const Eigen::Vector2d& p = *l.location();
+    return {p[0], p[1], l.orientation()};
+  }
+
+  const Eigen::Vector2d& p =
+      navigation_graph().get_waypoint(l.waypoint()).get_location();
+  return {p[0], p[1], l.orientation()};
+}
+
+//==============================================================================
+const std::string& RobotContext::map() const
+{
+  assert(!_location.empty());
+  return navigation_graph()
+      .get_waypoint(_location.front().waypoint()).get_map_name();
+}
+
+//==============================================================================
+rmf_traffic::Time RobotContext::now() const
+{
+  return rmf_traffic_ros2::convert(_node->now());
+}
+
+//==============================================================================
+const std::vector<rmf_traffic::agv::Plan::Start>& RobotContext::location() const
+{
+  return _location;
+}
+
+//==============================================================================
+rmf_traffic::schedule::Participant& RobotContext::schedule()
+{
+  return _schedule;
+}
+
+//==============================================================================
+const rmf_traffic::schedule::Participant& RobotContext::schedule() const
+{
+  return _schedule;
+}
+
+//==============================================================================
+const rmf_traffic::schedule::ParticipantDescription&
+RobotContext::description() const
+{
+  return _schedule.description();
+}
+
+//==============================================================================
+const rmf_traffic::agv::Graph& RobotContext::navigation_graph() const
+{
+  return _planner.get_configuration().graph();
+}
+
+//==============================================================================
+rmf_traffic::agv::Planner& RobotContext::planner()
+{
+  return _planner;
+}
+
+//==============================================================================
+const rmf_traffic::agv::Planner& RobotContext::planner() const
+{
+  return _planner;
+}
+
+//==============================================================================
+class RobotContext::NegotiatorSubscription
+{
+public:
+
+  NegotiatorSubscription(
+      std::shared_ptr<RobotContext> context,
+      rmf_traffic::schedule::Negotiator* negotiator)
+    : _context(context),
+      _negotiator(negotiator)
+  {
+    // Do nothing
+  }
+
+  ~NegotiatorSubscription()
+  {
+    const auto context = _context.lock();
+    if (!context)
+      return;
+
+    if (context && context->_negotiator == _negotiator)
+      context->_negotiator = nullptr;
+  }
+
+private:
+  std::weak_ptr<RobotContext> _context;
+  rmf_traffic::schedule::Negotiator* _negotiator;
+};
+
+//==============================================================================
+auto RobotContext::set_negotiator(
+    rmf_traffic::schedule::Negotiator* negotiator)
+-> std::shared_ptr<NegotiatorSubscription>
+{
+  _negotiator = negotiator;
+
+  return std::make_shared<NegotiatorSubscription>(
+        shared_from_this(), negotiator);
+}
+
+} // namespace agv
+} // namespace rmf_fleet_adapter
--- rmf_fleet_adapter/src/rmf_fleet_adapter/agv/RobotContext.hpp
+++ rmf_fleet_adapter/src/rmf_fleet_adapter/agv/RobotContext.hpp
@@ -2,12 +2,22 @@
 #define SRC__RMF_FLEET_ADAPTER__ROBOTCONTEXT_HPP
 
 #include <rmf_fleet_adapter/agv/RobotCommandHandle.hpp>
+#include <rmf_fleet_adapter/agv/RobotUpdateHandle.hpp>
+
+#include <rmf_traffic/schedule/Negotiator.hpp>
+#include <rmf_traffic/schedule/Participant.hpp>
+
+#include <rclcpp/node.hpp>
+
+#include <rxcpp/rx-observable.hpp>
 
 namespace rmf_fleet_adapter {
 namespace agv {
 
 //==============================================================================
 class RobotContext
+    : public std::enable_shared_from_this<RobotContext>,
+      public rmf_traffic::schedule::Negotiator
 {
 public:
 
@@ -16,18 +26,62 @@ public:
   /// available.
   std::shared_ptr<RobotCommandHandle> command();
 
+  /// Create a RobotUpdateHandle that reports to this RobotContext
+  std::shared_ptr<RobotUpdateHandle> make_updater();
+
   /// This is the robot's current (x, y, yaw) position.
   Eigen::Vector3d position() const;
 
   /// This is the map that the robot is on.
   const std::string& map() const;
 
+  /// Get the current time
+  rmf_traffic::Time now() const;
+
   /// This is the current "location" of the robot, which can be used to initiate
-  /// a planning job.
+  /// a planning job
   const std::vector<rmf_traffic::agv::Plan::Start>& location() const;
 
+  /// Get a mutable reference to the schedule of this robot
+  rmf_traffic::schedule::Participant& schedule();
+
+  /// Get a const-reference to the schedule of this robot
+  const rmf_traffic::schedule::Participant& schedule() const;
+
+  /// Get the schedule description of this robot
+  const rmf_traffic::schedule::ParticipantDescription& description() const;
+
+  /// Get the navigation graph used by this robot
+  const rmf_traffic::agv::Graph& navigation_graph() const;
+
+  /// Get a mutable reference to the planner for this robot
+  rmf_traffic::agv::Planner& planner();
+
+  /// Get a const-reference to the planner for this robot
+  const rmf_traffic::agv::Planner& planner() const;
+
+  class NegotiatorSubscription;
+
+  /// Set the schedule negotiator that will take responsibility for this robot.
+  /// Hold onto the returned subscription to remain the negotiator for this
+  /// robot.
+  std::shared_ptr<NegotiatorSubscription> set_negotiator(
+      rmf_traffic::schedule::Negotiator* negotiator);
+
+  struct Empty { };
+  rxcpp::observable<Empty>& observe_interrupt();
+
 private:
-  // TODO(MXG)
+  friend class RobotUpdateHandle;
+  std::weak_ptr<RobotCommandHandle> _command_handle;
+  std::vector<rmf_traffic::agv::Plan::Start> _location;
+  rmf_traffic::schedule::Participant _schedule;
+  rmf_traffic::schedule::Negotiator* _negotiator;
+  rmf_traffic::agv::Planner _planner;
+
+  // We're assuming that a RobotContextPtr is being held by the Node, so we
+  // shouldn't have to worry about the lifetime of the Node
+  rclcpp::Node* _node;
 };
 
 using RobotContextPtr = std::shared_ptr<RobotContext>;
