534943a9333e70dd0b8f6d96471f5b2d2e21dce6
koonpeng <koonpeng@openrobotics.org>
2020-06-09 11:42:55 +0800

    dock robot phase

---
+++ rmf_fleet_adapter/src/rmf_fleet_adapter/phases/DockRobot.cpp
@@ -0,0 +1,106 @@
+/*
+ * Copyright (C) 2020 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include "DockRobot.hpp"
+
+namespace rmf_fleet_adapter {
+namespace phases {
+
+//==============================================================================
+DockRobot::ActivePhase::ActivePhase(
+  agv::RobotContextPtr context,
+  std::string dock_name)
+  : _context{std::move(context)},
+    _dock_name{std::move(dock_name)}
+{
+  std::ostringstream oss;
+  oss << "Docking robot to " << _dock_name;
+  _description = oss.str();
+
+  _obs = rmf_rxcpp::make_job<Task::StatusMsg>([this](const auto& s)
+  {
+    _context->command()->dock(_dock_name, [s]()
+    {
+      s.on_completed();
+    });
+  });
+}
+
+//==============================================================================
+const rxcpp::observable<Task::StatusMsg>& DockRobot::ActivePhase::observe() const
+{
+  return _obs;
+}
+
+//==============================================================================
+rmf_traffic::Duration DockRobot::ActivePhase::estimate_remaining_time() const
+{
+  // TODO: implement
+  return rmf_traffic::Duration{0};
+}
+
+//==============================================================================
+void DockRobot::ActivePhase::emergency_alarm(bool on)
+{
+  // TODO: implement
+}
+
+//==============================================================================
+void DockRobot::ActivePhase::cancel()
+{
+  // no op
+}
+
+//==============================================================================
+const std::string& DockRobot::ActivePhase::description() const
+{
+  return _description;
+}
+
+//==============================================================================
+DockRobot::PendingPhase::PendingPhase(
+  agv::RobotContextPtr context,
+  std::string dock_name)
+  : _context{std::move(context)},
+    _dock_name{std::move(dock_name)}
+{
+  std::ostringstream oss;
+  oss << "Dock robot to " << _dock_name;
+  _description = oss.str();
+}
+
+//==============================================================================
+std::shared_ptr<Task::ActivePhase> DockRobot::PendingPhase::begin()
+{
+  return std::make_shared<DockRobot::ActivePhase>(_context, _dock_name);
+}
+
+//==============================================================================
+rmf_traffic::Duration DockRobot::PendingPhase::estimate_phase_duration() const
+{
+  // TODO: implement
+  return rmf_traffic::Duration{0};
+}
+
+//==============================================================================
+const std::string& DockRobot::PendingPhase::description() const
+{
+  return _description;
+}
+
+} // namespace phases
+} // namespace rmf_fleet_adapter
---
+++ rmf_fleet_adapter/src/rmf_fleet_adapter/phases/DockRobot.hpp
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2020 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#ifndef SRC__RMF_FLEET_ADAPTER__PHASES__DOCKROBOT_HPP
+#define SRC__RMF_FLEET_ADAPTER__PHASES__DOCKROBOT_HPP
+
+#include "../Task.hpp"
+#include "../agv/RobotContext.hpp"
+
+namespace rmf_fleet_adapter {
+namespace phases {
+
+struct DockRobot
+{
+  class ActivePhase : public Task::ActivePhase
+  {
+  public:
+
+    ActivePhase(
+      agv::RobotContextPtr context,
+      std::string dock_name);
+
+    const rxcpp::observable<Task::StatusMsg>& observe() const override;
+
+    rmf_traffic::Duration estimate_remaining_time() const override;
+
+    void emergency_alarm(bool on) override;
+
+    void cancel() override;
+
+    const std::string& description() const override;
+
+  private:
+
+    agv::RobotContextPtr _context;
+    std::string _dock_name;
+    std::string _description;
+    rxcpp::observable<Task::StatusMsg> _obs;
+  };
+
+  class PendingPhase : public Task::PendingPhase
+  {
+  public:
+
+    PendingPhase(
+      agv::RobotContextPtr context,
+      std::string dock_name);
+
+    std::shared_ptr<Task::ActivePhase> begin() override;
+
+    rmf_traffic::Duration estimate_phase_duration() const override;
+
+    const std::string& description() const override;
+
+  private:
+
+    agv::RobotContextPtr _context;
+    std::string _dock_name;
+    std::string _description;
+  };
+};
+
+} // namespace phases
+} // namespace rmf_fleet_adapter
+
+#endif // SRC__RMF_FLEET_ADAPTER__PHASES__DOCKROBOT_HPP
