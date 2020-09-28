3229a2fcdbe78dd94eaeeb31e723a09a81654f13
Michael X. Grey <grey@openrobotics.org>
2020-09-28 11:40:41 +0800

    Drafting blockade API

---
+++ rmf_traffic/include/rmf_traffic/blockade/Database.hpp
@@ -0,0 +1,16 @@
+#ifndef RMF_TRAFFIC__BLOCKADE__DATABASE_HPP
+#define RMF_TRAFFIC__BLOCKADE__DATABASE_HPP
+
+namespace rmf_traffic {
+namespace blockade {
+
+//==============================================================================
+class Database
+{
+
+};
+
+} // namespace blockade
+} // namespace rmf_traffic
+
+#endif // RMF_TRAFFIC__BLOCKADE__DATABASE_HPP
