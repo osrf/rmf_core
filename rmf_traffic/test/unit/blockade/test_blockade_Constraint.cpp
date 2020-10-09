455823a1fae31ac1ddb073f9a629bbba1cd3b146
Michael X. Grey <grey@openrobotics.org>
2020-10-09 13:26:17 +0800

    Adding more constraint tests

--- rmf_traffic/test/unit/blockade/test_blockade_Constraint.cpp
+++ rmf_traffic/test/unit/blockade/test_blockade_Constraint.cpp
@@ -239,6 +239,7 @@ SCENARIO("Test gridlock detection")
   const std::size_t B = 1;
   const std::size_t C = 2;
   const std::size_t D = 3;
+  const std::size_t E = 4;
 
   Blockers g;
 
@@ -466,5 +467,69 @@ SCENARIO("Test gridlock detection")
     state[B] = range(0, 0);
 
     simulate_all_sequences(g, no_gridlock, {5, 2, 2});
+  }
+
+  GIVEN("Three-way standoff with slash")
+  {
+    g[A][0] = blockage(D, hold_at(0), reach(1));
+    g[A][1] = blockage(C, hold_at(1), reach(2));
+    g[A][2] = blockage(B, hold_at(0), reach(2));
+    g[B][0] = blockage(A, hold_at(2), reach(3));
+    g[B][1] = blockage(C, hold_at(0), reach(2));
+    g[C][0] = blockage(B, hold_at(1), reach(2));
+    g[C][1] =
+        And(
+          blockage(A, hold_at(1), reach(3)),
+          blockage(D, std::nullopt, reach(1))
+        );
+    g[D][0] =
+        And(
+          blockage(A, hold_at(0), reach(2)),
+          blockage(C, hold_at(1), std::nullopt)
+        );
+
+    ConstConstraintPtr no_gridlock = compute_gridlock_constraint(g);
+    for (std::size_t i=A; i <= D; ++i)
+      CHECK(no_gridlock->dependencies().count(i));
+
+    simulate_all_sequences(g, no_gridlock, {3, 2, 2, 1});
+  }
+
+  GIVEN("Three-way standoff with an interior cross slash")
+  {
+    g[A][0] = blockage(C, hold_at(1), reach(2));
+    g[A][1] =
+        And(
+          blockage(B, hold_at(0), reach(2)),
+          blockage(D, hold_at(0), reach(2))
+        );
+    g[B][0] = blockage(A, hold_at(1), reach(2));
+    g[B][1] = blockage(C, hold_at(0), reach(2));
+    g[C][0] = blockage(B, hold_at(1), reach(2));
+    g[C][1] = blockage(A, hold_at(0), reach(2));
+    g[D][0] = blockage(A, hold_at(1), reach(2));
+    g[D][1] = blockage(E, hold_at(0), reach(1));
+    g[E][0] = blockage(D, hold_at(1), reach(2));
+
+    ConstConstraintPtr no_gridlock = compute_gridlock_constraint(g);
+    for (std::size_t i=A; i <= D; ++i)
+      CHECK(no_gridlock->dependencies().count(i));
+
+    CHECK_FALSE(no_gridlock->dependencies().count(E));
+
+    simulate_all_sequences(g, no_gridlock, {2, 2, 2, 2, 1});
+  }
+
+  GIVEN("Multiple head-to-head conflicts between two participants")
+  {
+    g[A][1] = blockage(B, hold_at(2), pass(3));
+    g[A][5] = blockage(B, std::nullopt, reach(2));
+    g[B][0] = blockage(A, hold_at(5), std::nullopt);
+    g[B][2] = blockage(A, hold_at(1), pass(3));
+
+    ConstConstraintPtr no_gridlock = compute_gridlock_constraint(g);
+    CHECK(no_gridlock->dependencies().empty());
+
+    simulate_all_sequences(g, no_gridlock, {8, 4});
   }
 }
