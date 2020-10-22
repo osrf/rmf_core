f593053e66e6906f9f75f83a4061fe5707d25e2a
Michael X. Grey <grey@openrobotics.org>
2020-10-22 17:59:45 +0800

    Starting on blockade participant class

--- rmf_traffic/include/rmf_traffic/blockade/Participant.hpp
+++ rmf_traffic/include/rmf_traffic/blockade/Participant.hpp
@@ -1,4 +1,35 @@
-#ifndef PARTICIPANT_HPP
-#define PARTICIPANT_HPP
+/*
+ * Copyright (C) 2020 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
 
-#endif // PARTICIPANT_HPP
+#ifndef RMF_TRAFFIC__BLOCKADE__PARTICIPANT_HPP
+#define RMF_TRAFFIC__BLOCKADE__PARTICIPANT_HPP
+
+namespace rmf_traffic {
+namespace blockade {
+
+//==============================================================================
+class Participant
+{
+public:
+};
+
+
+
+} // namespace blockade
+} // namespace rmf_traffic
+
+#endif // RMF_TRAFFIC__BLOCKADE__PARTICIPANT_HPP
