c564e7dd55a54a54471f018480d77bac0b7d5acf
Yadunund Vijay <yadunund@openrobotics.org>
2020-11-10 18:37:49 +0800

    Deleted old Delivery task implementation

--- rmf_fleet_adapter/src/rmf_fleet_adapter/tasks/Delivery.cpp
+++ rmf_fleet_adapter/src/rmf_fleet_adapter/tasks/Delivery.cpp
@@ -79,55 +79,6 @@ std::shared_ptr<Task> make_delivery(
     finish_state,
     request);
 }
-// std::shared_ptr<Task> make_delivery(
-//     const rmf_task_msgs::msg::Delivery& request,
-//     const agv::RobotContextPtr& context,
-//     rmf_traffic::agv::Plan::Start pickup_start,
-//     rmf_traffic::agv::Plan::Start dropoff_start)
-// {
-//   const auto& graph = context->navigation_graph();
-
-//   const auto pickup_wp =
-//       graph.find_waypoint(request.pickup_place_name)->index();
-
-//   Task::PendingPhases phases;
-//   phases.push_back(
-//         phases::GoToPlace::make(context, std::move(pickup_start), pickup_wp));
-
-//   phases.push_back(
-//         std::make_unique<phases::DispenseItem::PendingPhase>(
-//           context,
-//           request.task_id,
-//           request.pickup_dispenser,
-//           context->itinerary().description().owner(),
-//           request.items));
-
-//   const auto dropoff_wp =
-//       graph.find_waypoint(request.dropoff_place_name)->index();
-
-//   phases.push_back(
-//         phases::GoToPlace::make(context, std::move(dropoff_start), dropoff_wp));
-
-//   std::vector<rmf_ingestor_msgs::msg::IngestorRequestItem> ingestor_items;
-//   ingestor_items.reserve(request.items.size());
-//   for(auto& dispenser_item : request.items){
-//     rmf_ingestor_msgs::msg::IngestorRequestItem item{};
-//     item.type_guid = dispenser_item.type_guid;
-//     item.quantity = dispenser_item.quantity;
-//     item.compartment_name = dispenser_item.compartment_name;
-//     ingestor_items.push_back(std::move(item));
-//   }
-
-//   phases.push_back(
-//         std::make_unique<phases::IngestItem::PendingPhase>(
-//           context,
-//           request.task_id,
-//           request.dropoff_ingestor,
-//           context->itinerary().description().owner(),
-//           ingestor_items));
-
-//   return Task::make(request.task_id, std::move(phases), context->worker());
-// }
 
 } // namespace task
 } // namespace rmf_fleet_adapter
