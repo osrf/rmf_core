1cb9dff5bad7d9d9b3be8732192c4ddd37ef644b
Yadunund Vijay <yadunund@openrobotics.org>
2020-12-01 17:55:38 +0800

    Adjusted retreat threshold

--- rmf_fleet_adapter/src/rmf_fleet_adapter/TaskManager.cpp
+++ rmf_fleet_adapter/src/rmf_fleet_adapter/TaskManager.cpp
@@ -386,7 +386,7 @@ void TaskManager::retreat_to_charger()
     return;
 
   const double threshold_soc = _context->state_config().threshold_soc();
-  const double retreat_threshold = 1.5 * threshold_soc;
+  const double retreat_threshold = 1.2 * threshold_soc;
   const double current_battery_soc = _context->current_battery_soc();
 
   const auto task_planner_config = task_planner->config();
