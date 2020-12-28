8c6d9bba7a44494ea16c08a865113bcbe904a17c
Arjo Chakravarty <arjo@openrobotics.org>
2020-12-28 10:41:50 +0800

    Check idempotency of shape type

--- rmf_traffic_ros2/test/unit/test_ParticipantRegistry.cpp
+++ rmf_traffic_ros2/test/unit/test_ParticipantRegistry.cpp
@@ -1,6 +1,27 @@
 #include <rmf_utils/catch.hpp>
+#include <rmf_traffic_ros2/schedule/ParticipantRegistry.hpp>
 
-SCENARIO("just a random test")
+SCENARIO("Test idempotency of shape type")
 {
-  CHECK(1==2);
+  using namespace rmf_traffic_ros2;
+  YAML::Node node;
+
+  // Check "None"
+  auto serialized = serialize_shape_type(rmf_traffic_msgs::msg::ConvexShape::NONE);
+  node["type"] = serialized;
+  CHECK(shapetype(node["type"]) == rmf_traffic_msgs::msg::ConvexShape::NONE);
+
+  // Check "Box"
+  serialized = serialize_shape_type(rmf_traffic_msgs::msg::ConvexShape::BOX);
+  node["type"] = serialized;
+  CHECK(shapetype(node["type"]) == rmf_traffic_msgs::msg::ConvexShape::BOX);
+
+  // Check "Circle"
+  serialized = serialize_shape_type(rmf_traffic_msgs::msg::ConvexShape::CIRCLE);
+  node["type"] = serialized;
+  CHECK(shapetype(node["type"]) == rmf_traffic_msgs::msg::ConvexShape::CIRCLE);
+
+  REQUIRE_THROWS(serialize_shape_type(42));
+  node["type"] = "not a shape";
+  REQUIRE_THROWS(shapetype(node["type"]));
 }
