bd9a6020e5fdcc69945079aff90f6bddbe57f620
Arjo Chakravarty <arjo@openrobotics.org>
2020-12-30 10:30:54 +0800

    More comprehensive test for ParticipantRegistry

--- rmf_traffic_ros2/test/unit/test_ParticipantRegistry.cpp
+++ rmf_traffic_ros2/test/unit/test_ParticipantRegistry.cpp
@@ -29,9 +29,35 @@ SCENARIO("Test idempotency of shape type")
   REQUIRE_THROWS(shapetype(node["type"]));
 }
 
+
+bool operator==(const rmf_traffic::Profile p1, const rmf_traffic::Profile p2) 
+{
+  return convert(p1) == convert(p2);
+}
+
+bool operator==(
+  const rmf_traffic::schedule::ParticipantDescription desc1, 
+  const rmf_traffic::schedule::ParticipantDescription desc2)  
+{
+  return desc1.name() == desc2.name()
+    && desc1.owner() == desc2.owner()
+    && desc1.responsiveness() ==  desc2.responsiveness()
+    && desc1.profile() == desc2.profile();
+}
+
 SCENARIO("Test idempotency of ParticipantDescription.")
 {
+  //Lets create a bunch of participants
+  const auto shape = rmf_traffic::geometry::make_final_convex<
+    rmf_traffic::geometry::Circle>(1.0);
 
+  rmf_traffic::schedule::ParticipantDescription p1(
+    "participant 1",
+    "test_Participant",
+    rmf_traffic::schedule::ParticipantDescription::Rx::Responsive,
+    rmf_traffic::Profile{shape});
+
+  auto serialized = serialize(p1);
   
 }
 
@@ -92,7 +118,7 @@ SCENARIO("Participant registry restores participants from logger")
   
   ParticipantId participant_id1, participant_id2, participant_id3;
   
-  //Creating a new DB
+  WHEN("Creating a new DB without errors")
   {
     auto db1 = std::make_shared<Database>();
     ParticipantRegistry registry1(logger, db1);
@@ -100,18 +126,53 @@ SCENARIO("Participant registry restores participants from logger")
     participant_id2 = registry1.add_participant(p2);
     registry1.remove_participant(participant_id2);
     participant_id3 = registry1.add_participant(p3);
+
+    THEN("Restoring DB")
+    {
+      auto db2 = std::make_shared<Database>();
+      ParticipantRegistry registry2(logger, db2);
+      auto restored_participants = db2->participant_ids();
+      REQUIRE(restored_participants.count(participant_id1) > 0);
+      REQUIRE(restored_participants.count(participant_id2) == 0);
+      REQUIRE(restored_participants.count(participant_id3) > 0);
+      REQUIRE(restored_participants.size() == 2);
+
+      auto _p1 = db2->get_participant(participant_id1);
+      auto _p3 = db2->get_participant(participant_id3);
+
+      REQUIRE(*_p1 == p1);
+      REQUIRE(*_p3 == p3);
+    }
   }
 
-  //Restoring DB
+  WHEN("Creating a new DB with erroneous remove request")
   {
-    auto db2 = std::make_shared<Database>();
-    ParticipantRegistry registry2(logger, db2);
-    auto restored_participants = db2->participant_ids();
-    REQUIRE(restored_participants.count(participant_id1) > 0);
-    REQUIRE(restored_participants.count(participant_id2) == 0);
-    REQUIRE(restored_participants.count(participant_id3) > 0);
-    REQUIRE(restored_participants.size() == 2);
+    auto db1 = std::make_shared<Database>();
+    ParticipantRegistry registry1(logger, db1);
+    participant_id1 = registry1.add_participant(p1);
+    REQUIRE_THROWS(registry1.remove_participant(1000));
+    participant_id2 = registry1.add_participant(p2);
+    registry1.remove_participant(participant_id2);
+    participant_id3 = registry1.add_participant(p3);
+
+    THEN("Restoring DB")
+    {
+      auto db2 = std::make_shared<Database>();
+      ParticipantRegistry registry2(logger, db2);
+      auto restored_participants = db2->participant_ids();
+      REQUIRE(restored_participants.count(participant_id1) > 0);
+      REQUIRE(restored_participants.count(participant_id2) == 0);
+      REQUIRE(restored_participants.count(participant_id3) > 0);
+      REQUIRE(restored_participants.size() == 2);
+
+      auto _p1 = db2->get_participant(participant_id1);
+      auto _p3 = db2->get_participant(participant_id3);
+
+      REQUIRE(*_p1 == p1);
+      REQUIRE(*_p3 == p3);
+    }
   }
+  
 
   delete logger;
 }
